d9d177e9e622801cf045082e49f4c49279240050
Francesco Pontillo <francescopontillo@gmail.com>
2014-01-23 19:29:41 +0100

    Chapter 6, some of chapter 7
    location/2 and here/1 are now dynamic
    Added goto/1 and take/1 rules, with some "can-do" rules

--- src/kb.pl
+++ src/kb.pl
@@ -5,15 +5,16 @@ room('dining room').
 room(hall).
 room(cellar).
 
-% list of all the objects and related locations
-location(desk, office).
-location(apple, kitchen).
-location(flashlight, desk).
-location('washing machine', cellar).
-location(nani, 'washing machine').
-location(broccoli, kitchen).
-location(crackers, kitchen).
-location(computer, office).
+% list of all the objects and related locations (dinamically so we can remove some)
+:- assert(location(desk, office)).
+:- assert(location(apple, kitchen)).
+:- assert(location(flashlight, desk)).
+:- assert(location('washing machine', cellar)).
+:- assert(location(nani, 'washing machine')).
+:- assert(location(broccoli, kitchen)).
+:- assert(location(crackers, kitchen)).
+:- assert(location(computer, office)).
+
 
 % list of doors between rooms (one-way, for the moment)
 door(office, hall).
@@ -25,9 +26,12 @@ door('dining room', kitchen).
 % facts about properties of things the game player might try to eat
 edible(apple).
 edible(crackers).
+
+% me likey them broccoli
 tastes_yucky(broccoli).
 
 % at the beginning, the flashlight is off
 turned_off(flashlight).
-% the player is in the kitchen
-here(kitchen).
+
+% the player is in the kitchen (dinamically so we can remove some)
+:- asserta(here(kitchen)).
--- src/rules.pl
+++ src/rules.pl
@@ -1,6 +1,6 @@
 % a connection exists if there is a door from one door to another one or viceversa
-connect(DoorOut,DoorIn) :- door(DoorOut,DoorIn).
-connect(DoorIn,DoorOut) :- door(DoorOut,DoorIn).
+connect(DoorIn, DoorOut) :- door(DoorIn, DoorOut).
+connect(DoorIn, DoorOut) :- door(DoorOut, DoorIn).
 
 where_food(Food, Location) :- location(Food, Location), edible(Food).
 exists_food(Food) :- where_food(Food, ?).
@@ -22,3 +22,35 @@ look :- here(Here), write('You are in the '), write(Here), write('.'), nl,
 	
 look_in(Place) :- write(Place), write(' contains the following items:'), nl,
 	list_things(Place), nl.
+
+% go to a new place
+goto(Place) :- can_go(Place), move(Place), look.
+
+% checks if the player can move to another room
+% as soon as a connection is found, cut the rule (stop and be satisfied with the found value)
+can_go(Place) :- here(CurPlace), connect(Place, CurPlace), !.
+
+% if the previous can_go rule fails, we print a message and still fail
+can_go(Place) :-
+	write('Sorry, you can''t go to the '), write(Place), nl,
+	write('You can go to:'), nl, here(CurPlace), list_connections(CurPlace), nl,
+	fail.
+
+% move to another room, by removing the old here(...) rule and by adding the new one
+move(Place) :- retract(here(CurPlace)), asserta(here(Place)).
+
+% have/1, has one clause for each thing the game player has.
+% Initially, have/1 is not defined because the player is not carrying anything.
+
+% take/1, used by the player to grab something from the surrounding world
+take(Item) :- can_take(Item), take_object(Item).
+
+can_take(Item) :- here(CurPlace), location(Item, CurPlace), !.
+can_take(Item) :-
+	write('Sorry, you can''t take the '), write(Item), nl,
+	write('You can take:'), nl,
+	here(CurPlace), location(AvailableItem, CurPlace),
+	tab(2), write('- '), write(AvailableItem), nl,
+	fail.
+
+take_object(Item) :- asserta(have(Item)), retract(location(Item, _)).
