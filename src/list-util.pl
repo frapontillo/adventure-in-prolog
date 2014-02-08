1164d5cbf12b4f60a7ab8dded485893e69e9533e
Francesco Pontillo <francescopontillo@gmail.com>
2014-02-08 07:36:15 +0100

    Chapter 11: list (split/4, get_last/2, size/2, respond/1)

--- src/list-util.pl
+++ src/list-util.pl
@@ -29,7 +29,7 @@ append([Head|Tail], Element, [Head|NewTail]) :- append(Tail, Element, NewTail).
  * @param The resulting list
  */
 % removing an Element from an empty List returns an empty list
-remove([], Element, []).
+remove([], _, []).
 % if the Element is the Head of the list, return NewTail, which is the Tail after calling remove on it again
 remove([Element|Tail], Element, NewTail) :- remove(Tail, Element, NewTail), !.
 % if the Element is not the Head of the list, return the original Head and a NewTail, which is the Tail after calling remove on it again
@@ -42,11 +42,11 @@ remove([Head|Tail], Element, [Head|NewTail]) :- remove(Tail, Element, NewTail).
  * @param The element after the found one
  */
 % the ElementAfter after a list made by only Element does not exist
-find_after([Element], Element, After) :- false.
+find_after([Element], Element, _) :- false.
 % if the Element is the Head of the List, get the first element of the Tail
 find_after([Element|[After|_]], Element, After) :- true.
 % if the Element is not the Head of the List, recursively call find_after on the Tail
-find_after([Head|Tail], Element, After) :- find_after(Tail, Element, After).
+find_after([_|Tail], Element, After) :- find_after(Tail, Element, After).
 
 /*
  * split/4
@@ -55,10 +55,40 @@ find_after([Head|Tail], Element, After) :- find_after(Tail, Element, After).
  * @param The First resulting list
  * @param The Second resulting list; its first element will be Element
  */
-% if the Element is th Head of the List, the first list is empty, the second is the whole List
-% split(List, Element, First, Second) :- split_r(List, Element, First, Second, Temp).
-split([Element|Tail], Element, First, [Element|Tail]).
-split([Head|Tail], Element, [FHead|FTail],
-	
-split_r([Element|Tail], Element, First, NewFirst, [Element|Tail]).
-split_r([Head|Tail], Element, First, NewFirst, Second) :- append(First, Head, NewFirst), split_r(Tail, Element, [], NewFirst, Second).
+split(List, Element, First, Second) :- findall(X, split_get_first(List, Element, X), First), split_get_second(List, Element, Second).
+
+% If the Element is the Head, do nothing.
+split_get_first([Element|_], Element, Element) :- fail.
+% Return the Head of the list if it is different that the Element to search for
+split_get_first([Head|_], Element, Head) :- Element \= Head.
+% Then, analyze the remaining part of the list (the Tail)
+split_get_first([Head|Tail], Element, X) :- Element \= Head, split_get_first(Tail, Element, X).
+
+% if the Head matches the Element, return the whole list
+split_get_second([Element|Tail], Element, [Element|Tail]) :- !.
+% if the Element does no match the Head, appy the split to the Tail
+split_get_second([Head|Tail], Element, List) :- Head \= Tail, split_get_second(Tail, Element, List).
+
+/*
+ * get_last/2 Get the last element of a list
+ * @param List The List to search for
+ * @param Element The last element of the List
+ */
+get_last([Element], Element) :- !.
+get_last([_|Tail], Element) :- get_last(Tail, Element).
+
+/*
+ * size/2 Count the elements in a list
+ * @param List The List to count
+ * @param Count The List Count
+ */
+size([], 0) :- !.
+size([_], 1) :- !.
+size([Head|Tail], Count) :- size([Head], A), size(Tail, B), Count is (A+B), !.
+
+/*
+ * respond/1 Prints a List of terms
+ * @param List The List to write
+ */
+respond([]) :- !.
+respond([Head|Tail]) :- write(Head), respond(Tail).
